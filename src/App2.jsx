1d70f0a6fe619d1a9548fe0c0aeff652e6ae1945
adrianlazarpop <adrian.lazar.pop@gmail.com>
2025-11-23 19:54:14 +0200

    new App2.jsx file and updated main.jsx

---
+++ src/App2.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+class AppClass extends React.Component {
+  render() {
+    return (
+      <>
+        <h1>Hello world</h1>
+      </>
+    );
+  }
+}
+
+export default AppClass;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import AppClass from "./App2.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <AppClass />
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
