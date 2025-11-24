f1acc86515fd29508f3388dd1ee14687d8cfef9e
adrianlazarpop <adrian.lazar.pop@gmail.com>
2025-11-24 19:08:20 +0200

    practicing classes

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
-import AppClass from "./App2.jsx";
+// import App from "./App.jsx";
+// import AppClass from "./App2.jsx";
+import Hello from "./Hello.jsx";
+import "tachyons";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AppClass />
-    <App />
+    {/* <AppClass /> */}
+    {/* <App /> */}
+    <Hello greeting={"React Ninja"} />
   </StrictMode>
 );
