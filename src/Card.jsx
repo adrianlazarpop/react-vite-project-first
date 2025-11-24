a0c57a21c7f85a6d7a33de271feb89fcef625750
adrianlazarpop <adrian.lazar.pop@gmail.com>
2025-11-24 19:48:51 +0200

    robots update

---
+++ src/Card.jsx
@@ -0,0 +1,13 @@
+const Card = () => {
+  return (
+    <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+      <img src="https://robohash.org/test?500x500" alt="photo" />
+      <div>
+        <h2>Jane Doe</h2>
+        <p>jane.doe@gmail.com</p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,16 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 // import App from "./App.jsx";
 // import AppClass from "./App2.jsx";
-import Hello from "./Hello.jsx";
+// import Hello from "./Hello.jsx";
+import Card from "./Card";
 import "tachyons";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     {/* <AppClass /> */}
     {/* <App /> */}
-    <Hello greeting={"React Ninja"} />
+    <Card />
+    <Card />
+    <Card />
   </StrictMode>
 );
